6929675337f0b8e18454e85d7bc395df1bc0e7f8
pakoval <pakoval@softserveinc.com>
2024-02-15 11:38:19 +0200

    add tests for textarea

--- src/components/__tests__/TextArea.spec.ts
+++ src/components/__tests__/TextArea.spec.ts
@@ -9,14 +9,13 @@ describe("TextArea.vue", () => {
     wrapper = shallowMount(TextArea, {
       propsData: {
         placeholder: "Type text..",
-        value: "pop",
       },
     });
     vm = wrapper.vm;
   });
 
-  it("check existence of TextArea", () => {
-    expect(wrapper.exists()).toBeTruthy();
+  it("does a wrapper exist", () => {
+    expect(wrapper.isVueInstance).toBeTruthy();
   });
 
   it("check class of TextArea", () => {
@@ -25,5 +24,54 @@ describe("TextArea.vue", () => {
 
   it("check placeholder", () => {
     expect(wrapper.attributes("placeholder")).toBe("Type text..");
+  });
+
+  it("should set isReadonly", async () => {
+    expect(vm.isReadonly).toBeFalsy();
+    await wrapper.setProps({ isReadonly: true });
+    expect(vm.isReadonly).toBeTruthy();
+  });
+
+  describe("textareaAdjust", () => {
+    const smallText = "hello";
+
+    it("should call textareaAdjust", async () => {
+      // const textareaAdjustMock = jest.fn();
+      // vm.textareaAdjust = textareaAdjustMock;
+      // await wrapper.find("textarea").setValue("k");
+      // await wrapper.find("textarea").trigger("keydown", {
+      //   key: "k",
+      // });
+      // expect(textareaAdjustMock).toHaveBeenCalledWith(
+      //   expect.any(KeyboardEvent)
+      // );
+    });
+
+    it("should emit value", async () => {
+      await wrapper.setValue(smallText);
+      expect(wrapper.emitted().input).toEqual([[smallText]]);
+      expect(wrapper.emitted().input).toBeTruthy();
+    });
+
+    it("check resizing of fontSize", async () => {
+      const text =
+        "From friday i was starting test custom textarea component and I wrote couple tests for him. Faced with issue which related with typing in field. i'll try to overcome this soon. About main page still remaining 1 test for fixing. So today i'm planning to resolve these 2 issues at least and test custom Select component. If these tasks will take less day i'll pass one course at the end end end end end end";
+
+      const textareaEl = wrapper.element as HTMLTextAreaElement;
+      await wrapper.setValue(text);
+      expect(textareaEl.value).toEqual(text);
+      expect(textareaEl.style.fontSize).toBe("18px");
+
+      await wrapper.setValue(smallText);
+      expect(textareaEl.value).toEqual(smallText);
+      expect(textareaEl.style.fontSize).toBe("22px");
+    });
+  });
+
+  it("check setValue", async () => {
+    const textareaEl = wrapper.element as HTMLTextAreaElement;
+    expect(textareaEl.value).toEqual("");
+    await wrapper.setValue("hi");
+    expect(textareaEl.value).toEqual("hi");
   });
 });
